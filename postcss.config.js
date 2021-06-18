af8e1b7090bd0d60d990da6ea8f37b49d69be72b
Eniola Ajayi <ajayieniola16@gmail.com>
2021-06-18 12:55:48 +0100

    erase entire project

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   plugins: [require('tailwindcss'), require('autoprefixer')],
-// };
-const tailwindPlugin = require('tailwindcss');
-const autoPrefixerPlugin = require('autoprefixer');
-
-module.exports = {
-  plugins: [tailwindPlugin, autoPrefixerPlugin],
-};
