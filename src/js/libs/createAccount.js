e0e98d87605fe689a36d238a8462f6372a0165ce
Eniola Ajayi <ajayieniola16@gmail.com>
2021-07-10 16:44:00 +0100

    begin patient signup logic

--- src/js/libs/createAccount.js
+++ src/js/libs/createAccount.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { signUpUser } from "./signup";
+import { signUpUser, responseTemplate, getEmailFragments } from "./signup";
+// get elments from DOM ------------------------------------------------
+
 const mainContainer = document.querySelector(".acct__main");
 const createAcctForm = document.getElementsByClassName("createAccountForm")[0];
 const createAcctEmailInput =
@@ -10,75 +12,140 @@ const createAcctPasswordInput = document.getElementsByClassName(
 const createAcctPasswordInputCheck = document.getElementsByClassName(
   "createAccountConfirmPassword"
 )[0];
+
+const csuFormEl = document.querySelector(".csu-form");
+const csuFirstNameEl = document.querySelector(".csu-firstName");
+const csuLastNameEl = document.querySelector(".csu-lastName");
+const csuAddressEl = document.querySelector(".csu-address");
+const csuCityEl = document.querySelector(".csu-city");
+const csuStateEl = document.querySelector(".csu-state");
+const csuLGAEl = document.querySelector(".csu-lga");
+const csuDOBEl = document.querySelector(".csu-dob");
+const csuPhoneNoEl = document.querySelector(".csu-phoneno");
+const csuFormBtnEl = document.querySelector(".csu-form-button");
+const csuGenderEls = csuFormEl.elements["gender"];
+
+function csuElementAreAvailable() {
+  return (
+    csuFormEl &&
+    csuFirstNameEl &&
+    csuLastNameEl &&
+    csuAddressEl &&
+    csuCityEl &&
+    csuStateEl &&
+    csuLGAEl &&
+    csuDOBEl &&
+    csuPhoneNoEl &&
+    csuFormBtnEl &&
+    csuGenderEls
+  );
+}
+
+//------------------------------------------------------------
+let dataState = {
+  user: {
+    username: null,
+    email: null,
+    password: null,
+  },
+  phone_number: null,
+  first_name: null,
+  last_name: null,
+  other_name: null,
+  address: null,
+  gender: null,
+  date_of_birth: null,
+  city: null,
+  lga: null,
+  state: null,
+  address: null,
+};
+
 let email = "";
 let password = "";
 let confirmPassword = "";
+function setUserDetails(email, password, dataObj) {
+  const [name, domain] = getEmailFragments(email);
+  dataObj.user.username = name;
+  dataObj.user.email = email;
+  dataObj.user.password = password;
+}
 
-const responseTemplate = (type = "success") => {
-  return `
-<section class="acct__success">
-  ${
-    type === "success"
-      ? `
-      <figure class="acct__success-figure">
-        <img
-          src="./assets/Image/create-acct-success.svg"
-          alt="Account Created Successfully!"
-        />
-      </figure>
-      <h2 class="acct__success-heading">Confirm your account</h2>
-      <p class="acct__success-lead">
-        Thank you! <br />
-        Check your email for a link to finish setting up your account.
-      </p>`
-      : `<h2 class="acct__success-heading acct__success-heading--error">An Error Occured. Please Try Again!</h2>`
-  }
-    </section>
-`;
-};
+function passwordIsValid() {
+  return password === confirmPassword;
+}
+
+function assignValueFromInput(el, storage, validation) {
+  el.addEventListener("input", (e) => {
+    let value = el.value;
+    storage = value;
+    console.log(storage);
+  });
+}
+
 // Create Account Function
 export function CreateAccount() {
-  createAcctEmailInput.addEventListener("input", (e) => {
-    email = createAcctEmailInput.value;
-  });
-  createAcctPasswordInput.addEventListener("input", (e) => {
-    password = createAcctPasswordInput.value;
-  });
-  createAcctPasswordInputCheck.addEventListener("input", (e) => {
-    confirmPassword = createAcctPasswordInputCheck.value;
-  });
-
-  const getEmailFragments = (emailString) => emailString.split(/@(?=[^@]*$)/);
-  function passwordIsValid() {
-    return password === confirmPassword;
-  }
-  createAcctForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (passwordIsValid() && email != " " && password != " ") {
+  if (
+    createAcctEmailInput &&
+    createAcctPasswordInput &&
+    createAcctPasswordInputCheck &&
+    createAcctForm
+  ) {
+    createAcctEmailInput.addEventListener("input", (e) => {
+      email = createAcctEmailInput.value;
+    });
+    createAcctPasswordInput.addEventListener("input", (e) => {
+      password = createAcctPasswordInput.value;
+    });
+    createAcctPasswordInputCheck.addEventListener("input", (e) => {
+      confirmPassword = createAcctPasswordInputCheck.value;
+    });
+    createAcctForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      const [name, domain] = getEmailFragments(email);
-      // console.log(`${email} -- ${password} -- ${name} -- ${domain}`);
-      let data = JSON.stringify({
-        user: {
-          username: name,
-          email: email,
-          password: password,
-        },
-        phonenumber: "",
-      });
-     signUpUser(data)
-        .then(function (response) {
-          console.log(response);
-          mainContainer.innerHTML = responseTemplate();
-          window.location.pathname = "/signUp.html";
-        })
-        .catch(function (error) {
-          mainContainer.innerHTML = responseTemplate("error");
-          console.log(error);
-          console.log(error.response);
-        });
-    } else {
-      alert("Passwords dont match");
-    }
-  });
+      if (
+        passwordIsValid() &&
+        email != " " &&
+        password != " " &&
+        mainContainer
+      ) {
+        setUserDetails(email, password, dataState);
+        console.table(dataState);
+        window.location.pathname = "./signUp.html";
+      } else {
+        alert("Sorry Passwords don't match");
+      }
+    });
+  }
+
+  if (csuElementAreAvailable()) {
+    assignValueFromInput(csuFirstNameEl, dataState.first_name);
+    assignValueFromInput(csuLastNameEl, dataState.last_name);
+    assignValueFromInput(csuAddressEl, dataState.address);
+    assignValueFromInput(csuCityEl, dataState.city);
+    assignValueFromInput(csuPhoneNoEl, dataState.phone_number);
+    assignValueFromInput(csuStateEl, dataState.state);
+    assignValueFromInput(csuLGAEl, dataState.lga);
+    assignValueFromInput(csuDOBEl, dataState.date_of_birth);
+
+    csuFormEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+      csuFormBtnEl.textContent = "Creating...";
+      console.log(dataState);
+      // send data to backend
+      // redirect if need be
+    });
+  }
+}
+
+function signUp() {
+  signUpUser(data)
+    .then(function (response) {
+      console.log(response);
+      mainContainer.innerHTML = responseTemplate();
+    })
+    .catch(function (error) {
+      mainContainer.innerHTML = responseTemplate("error");
+      console.log(error);
+      console.log(error.response);
+    });
 }
--- src/js/libs/signup.js
+++ src/js/libs/signup.js
@@ -1,34 +1,52 @@
-const axios = require('axios');
-const REGISTER_PATIENT_URL = "https://e-care-be-api.herokuapp.com/api/v1/user/patient/register/";
-let signUpData = {
-    user:{
-        username:null,
-        email:null,
-        password:null,
+const axios = require("axios");
+const REGISTER_PATIENT_URL =
+  "https://e-care-be-api.herokuapp.com/api/v1/user/patient/register/";
+export function signUpUser(userData) {
+  let data = JSON.stringify(userData);
+  return axios({
+    method: "post",
+    url: REGISTER_PATIENT_URL,
+    data: data,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    phone_number:null,
-    first_name:null,
-    last_name:null,
-    other_name:null,
-    gender: null,
-    date_of_birth: null,
-    city: null,
-    lga: null,
-    state: null,
+    validateStatus: (status) => {
+      return true;
+    },
+  });
 }
 
-export function signUpUser(userData){
-    let data = JSON.stringify(userData);
-    return axios({
-        method: 'post',
-        url: REGISTER_PATIENT_URL,
-        data: data,
-        headers: { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        validateStatus: (status) => {
-            return true; 
-        },
-        });
-}
+/* 
+{ 
+'Accept': 'application/json',
+'Content-Type': 'application/json'
+},
+*/
+export const getEmailFragments = (emailString) =>
+  emailString.split(/@(?=[^@]*$)/);
+export const responseTemplate = (type = "success") => {
+  return `
+<section class="acct__success">
+  ${
+    type === "success"
+      ? `
+      <figure class="acct__success-figure">
+        <img
+          src="./assets/Image/create-acct-success.svg"
+          alt="Account Created Successfully!"
+        />
+      </figure>
+      <h2 class="acct__success-heading">Account Created Successfully!</h2>
+      <p class="acct__success-lead">
+        Nicely Done! <br />
+        Click <a href="./dashboard.html">here</a> to go to your dashboard.
+      </p>`
+      : `
+      <h2 class="acct__success-heading acct__success-heading--error">An Error Occured. Please Try Again!</h2>
+      <a href="./createAccount.html">Go Back</a>
+      `
+  }
+    </section>
+`;
+};
