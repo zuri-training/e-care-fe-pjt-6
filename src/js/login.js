752c6281f4b6eea650c84ccb3d5027992d5b822d
Eniola Ajayi <ajayieniola16@gmail.com>
2021-07-11 11:24:24 +0100

    refactor: clean up login file

--- src/js/login.js
+++ src/js/login.js
@@ -1,20 +1,14 @@
-const axios = require("axios");
-// grab form elements
+import axios from "axios";
+import { loginUser, storeUserID } from "./libs/auth/auth-util";
 const loginForm = document.querySelector(".loginForm");
 const messageContainer = document.querySelector(".loginFormMessage");
 const loginNameInput = document.querySelector(".loginName");
 const loginPasswordInput = document.querySelector(".loginPassword");
 const loginBtn = document.querySelector(".loginBtn");
-const LOGIN_API_URL = "https://e-care-be-api.herokuapp.com/api/v1/user/login/";
 let loginName = "";
 let loginPassword = "";
-const testData = JSON.stringify({
-    username: "austinoski",
-    password: "austinoski123"
-});
 
-export function Login() {
-if(loginNameInput && loginPasswordInput){
+if (loginNameInput && loginPasswordInput) {
   loginNameInput.addEventListener("input", () => {
     loginName = loginNameInput.value;
   });
@@ -22,49 +16,40 @@ if(loginNameInput && loginPasswordInput){
     loginPassword = loginPasswordInput.value;
   });
 }
-
+export function Login() {
   function displayMessage(message, type) {
-    if(messageContainer){
-    messageContainer.style.display = "block";
-    messageContainer.textContent = message;
-    messageContainer.classList.add(`loginFormMessage--${type}`);
-    setTimeout(() => {
-      messageContainer.style.display = "none";
-      messageContainer.classList.remove(`loginFormMessage--${type}`);
-    }, 5000);
-  } else{
-    return;
-  }
+    if (messageContainer) {
+      messageContainer.style.display = "block";
+      messageContainer.textContent = message;
+      messageContainer.classList.add(`loginFormMessage--${type}`);
+      setTimeout(() => {
+        messageContainer.style.display = "none";
+        messageContainer.classList.remove(`loginFormMessage--${type}`);
+      }, 5000);
+    } else {
+      return;
+    }
   }
   // on form submit
-  if(loginForm && loginBtn){
-  loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    loginBtn.textContent = "Sending...";
-    axios({
-      method: 'post',
-      url: LOGIN_API_URL,
-      data: {
+  if (loginForm && loginBtn) {
+    loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      loginBtn.textContent = "Sending...";
+      loginUser({
         username: loginName,
-        password: loginPassword
-      },
-      headers: { 
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-       },
-      validateStatus: (status) => {
-        // Idk know what this does 
-        return true; 
-      },
-    }).then(response => {
-        displayMessage("Success!", "success");
-        window.location.pathname = "./dashboard.html";
-        loginForm.reset();
-    }).catch(error => {
-         console.log(error);
-        displayMessage("An Error Occured - Please try again", "error");
-        loginBtn.textContent = "Login";
+        password: loginPassword,
+      })
+        .then((response) => {
+          storeUserID(response.data.uuid);
+          displayMessage("Success!", "success");
+          window.location.pathname = "./dashboard.html";
+          loginForm.reset();
+        })
+        .catch((error) => {
+          console.log(error);
+          displayMessage("An Error Occured - Please try again", "error");
+          loginBtn.textContent = "Login";
+        });
     });
-  });
-}
+  }
 }
