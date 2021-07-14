e0ed39a8874f1f37070e42262452292f992872a3
Eniola Ajayi <ajayieniola16@gmail.com>
2021-07-14 22:16:41 +0100

    safe commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { CreateAccount } from "./createAccount";
 import { Login } from "./login";
 import { Dashboard } from "./dashboard";
+import { DirectoryLogic } from "./directory";
+import { getCookie, setCookie } from "./libs/auth/auth-util";
 function toggleMobileNav() {
   const menuBtn = document.getElementById("menuBtn");
   const sideNav = document.getElementById("sideNav");
@@ -12,9 +14,36 @@ function toggleMobileNav() {
   }
 }
 
+function controlLoginState() {
+  if (getCookie("userid") && getCookie("access")) {
+    const loginActionBtns = document.querySelectorAll(".btnActionLogin");
+    const signUpActionBtns = document.querySelectorAll(".btnActionSignUp");
+    if (loginActionBtns) {
+      loginActionBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          window.location.pathname = "./dashboard.html";
+        });
+      });
+    }
+    if (signUpActionBtns) {
+      signUpActionBtns.forEach((btn) => {
+        btn.textContent = "Log Out";
+        btn.addEventListener("click", (e) => {
+          e.preventDefault();
+          setCookie("access", null, -2);
+          setCookie("userid", null, -2);
+          window.location.pathname = "./index.html";
+        });
+      });
+    }
+  }
+}
+
 // main point
 export default () => {
   console.log("We Live!");
+  controlLoginState();
+  DirectoryLogic();
   CreateAccount();
   Dashboard();
   toggleMobileNav();
